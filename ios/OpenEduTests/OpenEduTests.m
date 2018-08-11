c60a5ad32c5ac3d53bbf8e6b37ecd6da5ee890b6
eduard <eduard.open.w@gmail.com>
2018-08-11 11:27:10 +0800

    Update project

--- ios/OpenEduTests/OpenEduTests.m
+++ ios/OpenEduTests/OpenEduTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface courseTests : XCTestCase
+@interface OpenEduTests : XCTestCase
 
 @end
 
-@implementation courseTests
+@implementation OpenEduTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
